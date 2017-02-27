f6cfbbcfc43959dfd3c29a743e784ebc38d18a65
TaikiFnit <taikifnitspg@gmail.com>
2017-02-27 14:12:51 +0900

    add ViewController files

---
+++ TKZWHackathon2/MainTabBarController.swift
@@ -0,0 +1,42 @@
+//
+//  MainTabBarController.swift
+//  TKZWHackathon2
+//
+//  Created by TaikiFnit on 2017/02/27.
+//  Copyright © 2017年 白田光. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+ 
+        print("viewwill")
+//        self.performSegue(withIdentifier: "toSetGoal", sender: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
